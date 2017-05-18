e36112fa14f5de2432b55fb4b92be2f936e1601a
Hakob <jakob.avetis@gmail.com>
2017-05-18 16:07:49 +0400

    corrected random forest

--- Practical3/runner.py
+++ Practical3/runner.py
@@ -45,9 +45,9 @@ def evaluate_performance():
     NUMBER_OF_TREES = 10
     MAX_TREE_DEPTH_RF = 100
     MAX_TREE_DEPTH_TR = 100
-    RATIO_PER_TREE = 0.7
+    RATIO_PER_TREE = 0.25
 
-    for trial in range(1):
+    for trial in range(10):
         print('trial', trial + 1)
         idx = np.arange(n)
         np.random.seed(13)
@@ -62,8 +62,6 @@ def evaluate_performance():
         Xtrain = np.array([X[j] for j in range(len(X)) if (j % folds) != i])
         ytrain = np.array([y[j] for j in range(len(y)) if (j % folds) != i])
         train_data = np.column_stack((Xtrain, ytrain))
-        #idx = list(range(n))
-        #random.shuffle(idx)
 
         #print('dim of ytrain', ytrain.shape) # (240,1)
         #print('dim of Xtrain', Xtrain.shape) # (240,44)
@@ -71,20 +69,20 @@ def evaluate_performance():
         #print('dim of Xtest', Xtest.shape) # (27,44)
 
         # train the decision tree
-        #classifier_tree = DecisionTree(MAX_TREE_DEPTH_TR)
-        #classifier_tree.fit(train_data)
-        #y_pred = classifier_tree.predict(Xtest)
+        classifier_tree = DecisionTree(MAX_TREE_DEPTH_TR)
+        classifier_tree.fit(train_data)
+        y_pred = classifier_tree.predict(Xtest)
         #print('dim of y_pred', y_pred.shape)
-        #accuracy_tree = accuracy_score(ytest, np.array(y_pred))
-        #all_accuracies_tree.append(accuracy_tree)
+        accuracy_tree = accuracy_score(ytest, np.array(y_pred))
+        all_accuracies_tree.append(accuracy_tree)
 
         # train the random forest
-        #classifier_forest = RandomForest(NUMBER_OF_TREES, MAX_TREE_DEPTH_RF, RATIO_PER_TREE)
-        #classifier_forest.fit(train_data)
-        #y_pred_forest = classifier_forest.predict(Xtest)
+        classifier_forest = RandomForest(NUMBER_OF_TREES, MAX_TREE_DEPTH_RF, RATIO_PER_TREE)
+        classifier_forest.fit(train_data)
+        y_pred_forest = classifier_forest.predict(Xtest)
         #print('dim of y_pred_forest', y_pred_forest.shape)
-        #accuracy_forest = accuracy_score(ytest, np.array(y_pred_forest))
-        #all_accuracies_forest.append(accuracy_forest)
+        accuracy_forest = accuracy_score(ytest, np.array(y_pred_forest))
+        all_accuracies_forest.append(accuracy_forest)
         #print('forest has been classified')
 
         # train by logistic regression
@@ -107,21 +105,21 @@ def evaluate_performance():
         #print(y_pred_forest)
         
     # compute the training accuracies of the model
-    #meanDecisionTreeAccuracy = np.mean(all_accuracies_tree)
-    #stddevDecisionTreeAccuracy = np.std(all_accuracies_tree)
+    meanDecisionTreeAccuracy = np.mean(all_accuracies_tree)
+    stddevDecisionTreeAccuracy = np.std(all_accuracies_tree)
 
-    #meanRandomForestAccuracy = np.mean(all_accuracies_forest)
-    #stddevRandomForestAccuracy = np.std(all_accuracies_forest)
+    meanRandomForestAccuracy = np.mean(all_accuracies_forest)
+    stddevRandomForestAccuracy = np.std(all_accuracies_forest)
 
     meanLogisticRegressionAccuracy = np.mean(all_accuracies_log_reg)
     stddevLogisticRegressionAccuracy = np.std(all_accuracies_log_reg)
 
     # make certain that the return value matches the API specification
     stats = np.zeros((3, 2))
-    stats[0, 0] = 0#meanDecisionTreeAccuracy
-    stats[0, 1] = 0#stddevDecisionTreeAccuracy
-    stats[1, 0] = 0#meanRandomForestAccuracy
-    stats[1, 1] = 0#stddevRandomForestAccuracy
+    stats[0, 0] = meanDecisionTreeAccuracy
+    stats[0, 1] = stddevDecisionTreeAccuracy
+    stats[1, 0] = meanRandomForestAccuracy
+    stats[1, 1] = stddevRandomForestAccuracy
     stats[2, 0] = meanLogisticRegressionAccuracy
     stats[2, 1] = stddevLogisticRegressionAccuracy
     return stats
@@ -129,5 +127,5 @@ def evaluate_performance():
 if __name__ == "__main__":
     stats = evaluate_performance()
     print("Decision Tree Accuracy = ", stats[0, 0], " (", stats[0, 1], ")")
-    print("Random Forest Tree Accuracy = ", stats[1, 0], " (", stats[1, 1], ")")
+    print("Random Forest Accuracy = ", stats[1, 0], " (", stats[1, 1], ")")
     print("Logistic Reg. Accuracy = ", stats[2, 0], " (", stats[2, 1], ")")
